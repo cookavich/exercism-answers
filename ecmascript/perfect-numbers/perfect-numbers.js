b345d3bed503ccf55f611f551a0f1755352b38ae
cookavich <paul@lougeek.com>
2017-07-20 08:44:12 -0400

    All testes passing for perfect-numbers

---
+++ ecmascript/perfect-numbers/perfect-numbers.js
@@ -0,0 +1,22 @@
+class PerfectNumbers {
+    classify(int) {
+        if(int <= 0)
+            throw new Error('Classification is only possible for natural numbers.');
+        else if (this.getAliquotSum(int) === int)
+            return 'perfect';
+        else if (this.getAliquotSum(int) > int)
+            return 'abundant';
+        else
+            return 'deficient';
+    }
+
+    getAliquotSum(int) {
+        let aliquotSum = 0;
+        for (let i = 0; i < int; i++)
+            if (int % i === 0)
+                aliquotSum += i;
+        return aliquotSum;
+    }
+}
+
+export default PerfectNumbers;
