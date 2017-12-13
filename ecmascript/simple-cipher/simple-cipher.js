4507f868a35e40956b8600737552b1e1fec28da6
cookavich <paul@lougeek.com>
2017-12-13 16:23:35 -0500

    updates to various problems

--- ecmascript/simple-cipher/simple-cipher.js
+++ ecmascript/simple-cipher/simple-cipher.js
@@ -1,33 +1,60 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
 class Cipher {
-    constructor(key) {
-        this.key = key === undefined ? 'dddddddddd' : this._validateKey(key);
-    }
+  constructor(key) {
+    this.key = key === undefined ? 'dddddddddd' : this._validateKey(key);
+  }
 
-    encode(plaintext) {
-       const encoded = plaintext
-        .split('')
-        .map(letter => letter.charCodeAt(0) + 3);
-        return String.fromCharCode(...encoded);
+  encode(plaintext) {
+    if (this.key.length < plaintext.length) {
+      this._extendKey(plaintext);
     }
+    return plaintext.split('')
+      .map((letter, index) => this._rotateForward(letter, this.key[index]))
+      .join('');
+  }
 
-    decode(encoded) {
-        const decoded = encoded
-            .split('')
-            .map(letter => letter.charCodeAt(0) - 3);
-        return String.fromCharCode(...decoded);
-    }
+  decode(encoded) {
+    return encoded.split('')
+      .map((letter, index) => this._rotateBack(letter, this.key[index]))
+      .join('');
+  }
 
-    _validateKey(key) {
-        switch (true) {
-            case key.toUpperCase() === key:
-            case Number.isInteger(key):
-            case key.trim().length === 0:
-                throw new Error('Bad key');
-                break;
-            default:
-                return key;
-        }
+  _rotateForward(letter, keyEntry) {
+    const distance = alphabet.indexOf(keyEntry);
+    let newPosition = alphabet.indexOf(letter) + distance;
+    if (newPosition > 25) {
+      newPosition = newPosition - 26
     }
+    return alphabet[newPosition];
+  }
+
+  _rotateBack(letter, keyEntry) {
+    const distance = alphabet.indexOf(keyEntry);
+    let newPosition = alphabet.indexOf(letter) - distance;
+    if (newPosition < 0) {
+      newPosition = newPosition + 26
+    }
+    return alphabet[newPosition];
+  }
+
+  _validateKey(key) {
+    switch (true) {
+      case key.toUpperCase() === key:
+      case Number.isInteger(key):
+      case key.trim().length === 0:
+        throw new Error('Bad key');
+        break;
+      default:
+        return key;
+    }
+  }
+
+  _extendKey(plaintext) {
+    while (this.key.length < plaintext.length) {
+      this.key = this.key + this.key;
+    }
+  }
 }
 
 export default Cipher;
--- ecmascript/word-count/word-count.js
+++ ecmascript/word-count/word-count.js
@@ -1,13 +1,13 @@
 class Words {
   count(words) {
-    return words.replace(/[\t\n]+/g, ' ')
-      .split(' ')
-      .map(word => word.toLowerCase())
-      .filter(word => word.trim().length > 0)
+    return words.trim()
+      .toLowerCase()
+      .replace(/[\t\n]+/g, ' ')
+      .split(/\s+/)
       .reduce((tally, word) => {
         tally[word] = (+tally[word] || 0) + 1
         return tally;
-      }, {});
+      }, Object.create(null));
   }
 }
 
